03721809238306dbd614f5927f64cb99a6bc0e15
sumitisrani032 <sumitisrani032@gmail.com>
2026-06-03 15:01:13 +0530

    build: generate gem scaffold

---
+++ lib/walmart_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "walmart_api/version"
+
+module WalmartApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/walmart_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WalmartApi
+  VERSION = "0.1.0"
+end
